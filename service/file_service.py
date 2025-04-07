e9b7b1daf69f6a37c725aa6051fad83697545d43
megantebro <dazaizhongcun861@gmail.com>
2025-04-07 16:02:01 +0900

    画像が大きくなりすぎる問題を修正

--- service/file_service.py
+++ service/file_service.py
@@ -1,10 +1,11 @@
 import os
 import time
-
+from PIL import Image
 from werkzeug.utils import secure_filename
 
 
 def save_file(file,filename):
     filename = str(int(time.time())) + filename
     file.save(f"./static/uploads/{filename}")
+
     return filename
